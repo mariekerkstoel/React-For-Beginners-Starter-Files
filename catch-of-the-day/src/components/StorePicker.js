0f5c782b125a6d53c17fd27d85224cbeaabdd32a
mariekerkstoel <mariekerkstoel@gmail.com>
2018-04-18 15:11:15 +0100

    Create store picker component

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from 'react';
+import { render } from 'react-dom';
+
+class StorePicker extends React.Component {
+  render(){
+    return  (
+    <Fragment>
+      <p>Fish!!</p>
+      <form className='store-selector'>
+       <h2>Please enter a store</h2>
+       <input type='text' required placeholder='Store Name' />
+       <button type='submit'>Visit store -></button>
+      </form>
+    </Fragment>
+    )   
+  }
+}
+
+export default StorePicker;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,5 @@
-// let's go!
+import StorePicker from './components/StorePicker';
+import React from 'react';
+import { render } from 'react-dom';
+
+render(<StorePicker />, document.querySelector('#main'));
